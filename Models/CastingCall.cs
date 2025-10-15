d654bad24e20d3d023af5fd0037dbb161620e2fe
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-10-15 22:00:56 +1300

    added model props

---
+++ Models/CastingCall.cs
@@ -0,0 +1,17 @@
+﻿namespace CastingCallAPI.Models
+{
+    public class CastingCall
+    {
+        public Guid Id { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CastingDirector { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public string? Ethnicity { get; set; }
+        public string? Gender { get; set; }
+        public string? Age { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
